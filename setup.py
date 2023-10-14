0f841141d77b0a38f45489809b0eaa8ce9987197
williammincy <will@wmdigital.com>
2023-10-14 16:39:30 -0400

    Adding email and issue classes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.9.1'
+VERSION = '0.9.2'
 DESCRIPTION = 'API class for Curated.co'
 LONG_DESCRIPTION = 'This package provides a Python interface for the Curated API. With it, you can easily manage issues, links, and subscribers within publications.'
 
--- src/curatedapi_wm/curatedapi.py
+++ src/curatedapi_wm/curatedapi.py
@@ -1,9 +1,10 @@
 import requests
 import json
+from urllib.parse import urlencode
 
 
 class CuratedLink:
-    def __init__(self, url, title=None, description=None, image=None, category=None, id=None):
+    def __init__(self, url:str, title:str=None, description:str=None, image:str=None, category:str=None, id:int=None):
         """
         Represents a curated link.
 
@@ -65,7 +66,7 @@ class CuratedLink:
 
 
 class CuratedCategory:
-    def __init__(self, code=None, name=None, sponsored=False, limit=None):
+    def __init__(self, code:str=None, name:str=None, sponsored:bool=False, limit:int=None):
         self.code = code
         self.name = name
         self.sponsored = sponsored
@@ -107,9 +108,125 @@ class CuratedCategory:
         }
         return json.dumps(category_data)
 
+class CuratedIssuesResponse:
+    def __init__(self, page:int, total_pages:int, total_results:int, data:dict):
+        """
+        Represents a curated issue response.
+
+        Parameters:
+            page (int): The page number for the data returned in this call.
+            total_pages (int):  How many pages are available using this page size.
+            total_results (int): The total number of results which are available.
+            data (dict): A dictionary of CuratedIssue instances.
+        """
+        self.page = page
+        self.total_pages = total_pages
+        self.total_results = total_results 
+        self.data = data
+
+    def __str__(self):
+        """Returns a string representation of the CuratedLink."""
+        return f"Page: {self.page}\nTotal Pages: {self.total_pages}\nTotal Results: {self.total_results}\nData: {self.data}\n"
+
+    @classmethod
+    def from_json(cls, json_data):
+        """
+        Create a new instance of CuratedIssuesResponse based on a JSON object.
+
+        Parameters:
+            json_data (dict): A JSON object containing data for CuratedIssuesResponse attributes.
+
+        Returns:
+            CuratedIssuesResponse: A new instance of CuratedIssuesResponse with data from the JSON object.
+        """
+        return cls(
+            page=json_data.get("page"),
+            total_pages=json_data.get("total_pages"),
+            total_results=json_data.get("total_results"),
+            data=[CuratedIssue.from_json(issue)
+                    for issue in json_data.get("data")]
+        )
+
+    def to_json(self):
+        """
+        Return the object as JSON.
+
+        Returns:
+            str: JSON representation of the CuratedIssuesResponse instance.
+        """
+        link_data = {
+            "page": self.page,
+            "total_pages": self.total_pages,
+            "total_results": self.total_results,
+            "data": [CuratedIssue.from_json(issue)
+                     for issue in self.data]
+        }
+        return json.dumps(link_data)
+
+class CuratedIssue:
+    def __init__(self, id:int, number:int, published_at:str=None, summary:str=None, url:str=None, updated_at :str=None):
+        """
+        Represents a curated link.
+
+        Parameters:
+            id (int): Local identifier for the issue instance.
+            number (int): The issue number.
+            published_at (str, optional): The date that the issue was published in ISO 8601 format.
+            summary (str, optional): The summary for the issue instance.
+            url (str, optional): The public URL associated with the issue.
+            updated_at (str, optional): The date that the issue was last updated (may be after publishing) in ISO 8601 format.
+        """
+        self.id = id
+        self.number = number
+        self.published_at = published_at
+        self.summary = summary
+        self.url  = url 
+        self.updated_at = updated_at
+
+    def __str__(self):
+        """Returns a string representation of the CuratedIssue."""
+        return f"ID: {self.id}\nIssue Number: {self.number}\nSummary: {self.summary}\nURL: {self.url}\Published Date: {self.published_at}\Last nUpdated: {self.updated_at}\n"
+
+    @classmethod
+    def from_json(cls, json_data):
+        """
+        Create a new instance of CuratedIssue based on a JSON object.
+
+        Parameters:
+            json_data (dict): A JSON object containing data for CuratedIssue attributes.
+
+        Returns:
+            CuratedIssue: A new instance of CuratedIssue with data from the JSON object.
+        """
+        return cls(
+            id=json_data.get("id"),
+            number=json_data.get("number"),
+            published_at=json_data.get("published_at"),
+            summary=json_data.get("summary"),
+            url=json_data.get("url"),
+            updated_at=json_data.get("updated_at")
+        )
+
+    def to_json(self):
+        """
+        Return the object as JSON.
+
+        Returns:
+            str: JSON representation of the CuratedIssue instance.
+        """
+        link_data = {
+            "id": self.id,
+            "number": self.number,
+            "published_at": self.published_at,
+            "summary": self.summary,
+            "url": self.url,
+            "updated_at": self.updated_at
+        }
+        return json.dumps(link_data)
+
 
 class CuratedApi:
-    def __init__(self, api_key):
+    def __init__(self, api_key:str):
         """
         Initialize the CuratedApi class with the provided API key.
 
@@ -128,7 +245,7 @@ class CuratedApi:
         """Returns the API key."""
         return self.api_key
 
-    def set_publication_id(self, publication_id):
+    def set_publication_id(self, publication_id:int):
         """
         Sets the publication ID.
 
@@ -137,18 +254,13 @@ class CuratedApi:
         """
         self.publication_id = publication_id
 
-    def get_categories_url(self, publication_id=None):
+    def get_categories_url(self):
         """
         Returns the URL for accessing categories for the specified publication or the stored publication ID.
-
-        Parameters:
-            publication_id (str, optional): The ID of the publication.
 
         Returns:
             str: The concatenated URL string.
         """
-        if publication_id:
-            self.publication_id = publication_id
 
         if not self.publication_id:
             raise ValueError("Publication ID has not been set.")
@@ -169,6 +281,19 @@ class CuratedApi:
 
         publication_url = f"{self.base_url}/publications/{self.publication_id}/links"
         return publication_url
+    
+    def get_email_subscribers_url(self):
+        """
+        Returns the email subscribers URL for the stored publication ID.
+
+        Returns:
+            str: The concatenated URL string.
+        """
+        if not self.publication_id:
+            raise ValueError("Publication ID has not been set.")
+
+        subscribers_url = f"{self.base_url}/publications/{self.publication_id}/email_unsubscribers"
+        return subscribers_url
     
     def get_publication_issues_url(self):
         """
@@ -196,43 +321,29 @@ class CuratedApi:
         publication_url = f"{self.base_url}/publications"
         return publication_url
 
-    def get_links_url(self, publication_id=None):
+    def get_links_url(self):
         """
         Returns the URL for getting link not associated with a publication
 
         Parameters:
             link_id (str): The ID of the link.
-            publication_id (str, optional): The ID of the publication.
 
         Returns:
             str: The concatenated URL string.
         """
-        if publication_id:
-            self.publication_id = publication_id
-
-        if not self.publication_id:
-            raise ValueError("Publication ID has not been set.")
-
         links_url = f"{self.base_url}/publications/{self.publication_id}/links"
         return links_url
 
-    def get_link_url(self, link_id, publication_id=None):
+    def get_link_url(self, link_id:int):
         """
         Returns the URL for the specified link ID and optionally the publication ID.
 
         Parameters:
             link_id (str): The ID of the link.
-            publication_id (str, optional): The ID of the publication.
 
         Returns:
             str: The concatenated URL string.
         """
-        if publication_id:
-            self.publication_id = publication_id
-
-        if not self.publication_id:
-            raise ValueError("Publication ID has not been set.")
-
         link_url = f"{self.base_url}/publications/{self.publication_id}/links/{link_id}"
         return link_url
 
@@ -245,13 +356,7 @@ class CuratedApi:
         }
         return headers
 
-    def request_all_publications(self, publication_id=None):
-        if publication_id:
-            self.publication_id = publication_id
-
-        if not self.publication_id:
-            raise ValueError("Publication ID has not been set.")
-
+    def request_all_publications(self):
         url = self.get_publications_url()
         headers = self.get_request_headers()
         response = requests.get(url, headers=headers)
@@ -262,22 +367,13 @@ class CuratedApi:
         else:
             response.raise_for_status()
 
-    def request_all_categories(self, publication_id=None):
+    def request_all_categories(self):
         """
         Requests all categories from the publication using a GET HTTP request.
-
-        Parameters:
-            publication_id (str, optional): The ID of the publication.
 
         Returns:
             list: A list of CuratedCategory instances representing the categories.
         """
-        if publication_id:
-            self.publication_id = publication_id
-
-        if not self.publication_id:
-            raise ValueError("Publication ID has not been set.")
-
         url = self.get_categories_url()
         headers = self.get_request_headers()
         response = requests.get(url, headers=headers)
@@ -290,22 +386,13 @@ class CuratedApi:
         else:
             response.raise_for_status()
 
-    def request_all_links(self, publication_id=None):
+    def request_all_links(self):
         """
         Sends a GET request to retrieve all links from the specified publication or the stored publication ID.
-
-        Parameters:
-            publication_id (str, optional): The ID of the publication.
 
         Returns:
             dict: JSON response containing the links data.
         """
-        if publication_id:
-            self.publication_id = publication_id
-
-        if not self.publication_id:
-            raise ValueError("Publication ID has not been set.")
-
         url = self.get_links_url()
         headers = self.get_request_headers()
 
@@ -318,24 +405,35 @@ class CuratedApi:
             return links
         else:
             response.raise_for_status()
+            
+    def request_all_subscribers(self):
+        """
+        Sends a GET request to retrieve all email subscribers from the specified publication or the stored publication ID.
 
-    def request_specific_link(self, link_id, publication_id=None):
+        Returns:
+            dict: JSON response containing the links data.
+        """
+        url = self.get_email_subscribers_url()
+        headers = self.get_request_headers()
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            links_data = response.json()
+            return links_data
+        else:
+            response.raise_for_status()
+
+    def request_specific_link(self, link_id:int):
         """
         Sends a GET request to retrieve a specific link from the specified publication or the stored publication ID.
 
         Parameters:
             link_id (str): The ID of the link.
-            publication_id (str, optional): The ID of the publication.
 
         Returns:
             CuratedLink: An instance of the CuratedLink class representing the specific link.
         """
-        if publication_id:
-            self.publication_id = publication_id
-
-        if not self.publication_id:
-            raise ValueError("Publication ID has not been set.")
-
         url = self.get_link_url(link_id, self.publication_id)
         headers = self.get_request_headers()
 
@@ -348,7 +446,7 @@ class CuratedApi:
         else:
             response.raise_for_status()
 
-    def create_curated_link(self, curated_link):
+    def create_curated_link(self, curated_link:CuratedLink):
         """
         Creates a new curated link by sending a POST request to the publication URL.
 
@@ -356,24 +454,20 @@ class CuratedApi:
             curated_link (CuratedLink): An instance of the CuratedLink class representing the link to create.
 
         Raises:
-            ValueError: If the URL or title of the CuratedLink instance is None.
+            ValueError: If the URL of the CuratedLink instance is None.
 
         Returns:
-            dict: JSON response containing the newly created link data.
+            CuratedLink: The newly created link data as an instance of CuratedLink.
         """
         if curated_link.url is None:
             raise ValueError("URL of the CuratedLink instance cannot be None.")
-        if curated_link.title is None:
-            raise ValueError(
-                "Title of the CuratedLink instance cannot be None.")
 
         url = self.get_links_url()
         headers = self.get_request_headers()
-        query = "?url=" + curated_link.url + "&title=" + curated_link.title + \
-            "&description='" + curated_link.description + "'&image=" + \
-                curated_link.image + "&category=" + curated_link.category
 
-        response = requests.post(url+query, headers=headers)
+        query = "?" + urlencode(curated_link.to_json)
+
+        response = requests.post(url + query, headers=headers)
 
         if response.status_code == 201:
             response_data = response.json()
@@ -383,23 +477,48 @@ class CuratedApi:
             print(response)
             response.raise_for_status()
 
-    def delete_specific_link(self, link_id, publication_id=None):
+    def subscribe_email(self, email:str, sync:bool=False):
+        """
+        Creates a new curated link by sending a POST request to the publication URL.
+
+        Parameters:
+            email (str): Email address we want to subscribe to the publication.
+            sync (bool, optional): Set to true to bypass double opt-in settings, default is False
+
+        Raises:
+            ValueError: If the email is None.
+
+        Returns:
+            dict: JSON response containing the newly subscriber email data.
+        """
+        if email is None:
+            raise ValueError("email cannot be None.")
+        
+        url = self.get_email_subscribers_url()
+        headers = self.get_request_headers()
+        data = {
+            "email": email,
+            "sync": sync
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+        
+        if response.status_code == 201:
+            return response.json()
+        else:
+            print(response)
+            response.raise_for_status()
+
+    def delete_specific_link(self, link_id:int):
         """
         Sends a DELETE request to remove a specific link from the specified publication or the stored publication ID.
 
         Parameters:
             link_id (str): The ID of the link to delete.
-            publication_id (str, optional): The ID of the publication.
 
         Returns:
             str: A message indicating the success of the deletion.
         """
-        if publication_id:
-            self.publication_id = publication_id
-
-        if not self.publication_id:
-            raise ValueError("Publication ID has not been set.")
-
         url = self.get_link_url(link_id, self.publication_id)
         headers = self.get_request_headers()
 
@@ -412,23 +531,16 @@ class CuratedApi:
         else:
             response.raise_for_status()
 
-    def update_specific_link(self, curated_link, publication_id=None):
+    def update_specific_link(self, curated_link:CuratedLink):
         """
         Updates a specific curated link using a PUT HTTP request.
 
         Parameters:
             curated_link (CuratedLink): The CuratedLink instance to be updated.
-            publication_id (str, optional): The ID of the publication.
 
         Returns:
             CuratedLink: The updated CuratedLink instance.
         """
-        if publication_id:
-            self.publication_id = publication_id
-
-        if not self.publication_id:
-            raise ValueError("Publication ID has not been set.")
-
         if not curated_link.id:
             raise ValueError("CuratedLink's id has not been set.")
 
@@ -440,7 +552,36 @@ class CuratedApi:
 
         if response.status_code == 200:
             link_data = response.json()
-            # updated_link = CuratedLink.from_json(link_data)
             return link_data
+        else:
+            response.raise_for_status()
+
+    def request_all_issues(self, per_page:int=10, state:str="draft", page:int=None, stats:bool=False):
+        """
+        Sends a GET request to retrieve all links from the specified publication or the stored publication ID.
+
+        Parameters:
+            per_page (int, optional): Total issues returned per page, maximum 250.
+            state (str, optional): Status of issues returned, defaults to returneing "draft" issues .
+            page (int, optional): Current page within total number of issues.
+            stats (bool, optional): Should issue stats be returned with responses, defaults to False.
+
+        Returns:
+            dict: JSON response containing the links data.
+        """
+        url = self.get_publication_issues_url()
+        headers = self.get_request_headers()
+        query = "?per_page=" + per_page + "&state=" + state + \
+            "&stats='" + stats
+        if page is not None:
+            query = query + "&page=" + page
+
+        response = requests.get(url + query, headers=headers)
+
+        if response.status_code == 200:
+            links_data = response.json()
+            links = [CuratedLink.from_json(link_data)
+                     for link_data in links_data]
+            return links
         else:
             response.raise_for_status()
